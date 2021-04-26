5e6dd32273f6b6f3e21ca8c519975ab9267e2cef
Marta Wojdat <marjow4@gmail.com>
2021-04-26 14:21:30 +0200

    Change named functions to anonymous

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 {
-	function playGame(playerInput){
+	const playGame = function(playerInput){
 	clearMessages()
 
-	function getMoveName(argMoveId){
+	const getMoveName = function(argMoveId){
 		  if(argMoveId == 1){
 		    return 'kamień';
 		  } else if(argMoveId == 2){
@@ -29,7 +29,7 @@
 
 	printMessage('Twój ruch to: ' + playerMove);
 
-	function displayResult(argComputerMove, argPlayerMove){
+	const displayResult = function(argComputerMove, argPlayerMove){
 		if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
 	  		return 'Ty wygrywasz!';
 		} else if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
